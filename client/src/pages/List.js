0911e163372cd4f2d06140753997024c0ae041bb
Dalamo <dalamo90@gmail.com>
2018-06-20 11:37:46 -0500

    pages linked

---
+++ client/src/pages/List.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class List extends Component {
+    state = {
+
+    }
+
+    render () {
+        return (
+            <h1>The List page!</h1>
+        )
+    }
+}
+export default List
