6e0e8a93e4e56232dd80be001e7d4309af179334
Dalamo <dalamo90@gmail.com>
2018-06-20 12:21:00 -0500

    fixed divs

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -2,16 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Welcome = () => (
-    <div className="container">
+<div className="container">
+    <div className="row mt-5">
         <div className="col-12">
             <div className="jumbotron">
-                <h1 className="display-4">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr className="my-4">
-                    <Link to="/list" className="btn btn-primary btn-lg">Go to my</Link>
-                </hr>
+                <h1 className="display-4">Greetings!</h1>
+                <p className="lead">Welcome to this app!</p>
+                <hr className="my-4" />
+                <Link to="/list" className="btn btn-primary btn-lg">Go to my</Link>
             </div>
         </div>
     </div>
+</div>  
 )
 export default Welcome;
